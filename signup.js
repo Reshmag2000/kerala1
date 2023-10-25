e81133864f884cbcce3587543940f9661ff9f270
Reshmag2000 <145742035+Reshmag2000@users.noreply.github.com>
2023-10-25 13:00:19 +0530

    new change
    change

--- signup.js
+++ signup.js
@@ -1,86 +1,181 @@
-let mail=document.getElementById('mail');
-let pswd=document.getElementById('password');
-let strengthText=document.getElementById('strengthText');
-let phoneNumber=document.getElementById('phoneNumber');
-var cnpswd=document.getElementById('confirmpassword');
+//First Name
+let firstNameInput = document.getElementById("first-name-input");
+let firstNameError = document.getElementById("first-name-error");
+let emptyFirstNameError = document.getElementById("empty-first-name");
 
-var hasUppercase =/[A-Z]/.test(password);
-var hasLowercase =/[a-z]/.test(password);
-var hasNumber =/[0-9]/.test(password);
-var hasSpecialChar = /[!@#$%^&*]/.test(password);
-let regex=/^([A-Za-z0-9\#_.-]+)@([a-zA-Z0-9\-]+).([a-z]{2,3})?$/
-let phoneRegex = /^\d{10}$/.test(phoneNumber);
+//Last name
+let lastNameInput = document.getElementById("last-name-input");
+let lastNameError = document.getElementById("last-name-error");
+let emptyLastNameError = document.getElementById("empty-last-name");
 
+//Phone
+let phoneInput = document.getElementById("phone");
+let phoneError = document.getElementById("phone-error");
+let emptyPhoneError = document.getElementById("empty-phone");
 
-function verifymail()
-{
-    if(regex.test(mail.value))
-    {
-        alert('email is valid');
-    }
-    else{
-        alert('email is not valid');
-    }
-}
+//Email
+let emailInput = document.getElementById("email");
+let emailError = document.getElementById("email-error");
+let emptyEmailError = document.getElementById("empty-email");
 
+//Password
+let passwordInput = document.getElementById("password");
+let passwordError = document.getElementById("password-error");
+let emptyPasswordError = document.getElementById("empty-password");
 
+//Verify Password
+let verifyPasswordInput = document.getElementById("verify-password");
+let verifyPasswordError = document.getElementById("verify-password-error");
+let emptyVerifyPasswordError = document.getElementById("empty-verify-password");
 
+//Submit
+let submitButton = document.getElementById("submit-button");
 
+//Valid
+let validClasses = document.getElementsByClassName("valid");
+let invalidClasses = document.getElementsByClassName("error");
 
+//Password Verification
+const passwordVerify = (password) => {
+  const regex =
+    /^(?=.+[a-z])(?=.+[A-Z])(?=.+[0-9])(?=.+[\$\%\^\&\!@\#\*\(\)\+\=`~\?\>\<])/;
+  return regex.test(password) && password.length >= 8;
+};
 
-function verifypassword()
-{
-    if(!hasUppercase || !hasLowercase || !hasNumber) 
-    {
-        alert("password must contain atleast one uppercase letter, one lowercase letter, and one number");
-    }
-    if(password.value!= confirmpassword.value)
-    {
-        alert('Password do not match');
-    }
-    if(password.length<8)
-    {
-        alert('password must be atleast 8 characters long');
-    }
-}
+//Text verification (if input contains only text)
+const textVerify = (text) => {
+  const regex = /^[a-zA-Z]{3,}$/;
+  return regex.test(text);
+};
 
-function checkPasswordStrength()
-{
-    var strength = 0;
+//Phone number verification
+const phoneVerify = (number) => {
+  const regex = /^[0-9]{10}$/;
+  return regex.test(number);
+};
 
-    if (password.length >= 8) {
-        strength++;
-    }
-    if (hasUppercase && hasLowercase) {
-        strength++;
-    }
-    if (hasNumber) {
-        strength++;
-    }
-    if (hasSpecialChar) {
-        strength++;
-    }
+//Email verification
+const emailVerify = (input) => {
+  const regex = /^[a-z0-9_]+@[a-z]{3,}\.[a-z\.]{3,}$/;
+  return regex.test(input);
+};
 
-    // Update the strengthText element based on the password strength
-    if (strength == 4) {
-        strengthText.textContent = "Strong";
-        strengthText.className = "strong";
-    } else if (strength >= 2) {
-        strengthText.textContent = "Medium";
-        strengthText.className = "medium";
-    } else {
-        strengthText.textContent = "Poor";
-        strengthText.className = "poor";
-    }
-}
-
-
-function validatePhoneNumber() {
-  //var phoneNumber = "1234567890";
-  if (phoneRegex.test(phoneNumber.value)) 
-  {
-    console.log("Valid phone number");
+//For empty input - accepts(input,empty error for that input and other errors)
+const emptyUpdate = (
+  inputReference,
+  emptyErrorReference,
+  otherErrorReference
+) => {
+  if (!inputReference.value) {
+    //input is null/empty
+    emptyErrorReference.classList.remove("hide");
+    otherErrorReference.classList.add("hide");
+    inputReference.classList.add("error");
   } else {
-    console.log("Invalid phone number");
+    //input has some content
+    emptyErrorReference.classList.add("hide");
   }
-}
+};
+
+//For error styling and displaying error message
+const errorUpdate = (inputReference, errorReference) => {
+  errorReference.classList.remove("hide");
+  inputReference.classList.remove("valid");
+  inputReference.classList.add("error");
+};
+
+//For no errors
+const validInput = (inputReference) => {
+  inputReference.classList.remove("error");
+  inputReference.classList.add("valid");
+};
+
+//First name
+firstNameInput.addEventListener("input", () => {
+  if (textVerify(firstNameInput.value)) {
+    //If verification returns true
+    firstNameError.classList.add("hide");
+    validInput(firstNameInput);
+  } else {
+    //for false
+    errorUpdate(firstNameInput, firstNameError);
+    //empty checker
+    emptyUpdate(firstNameInput, emptyFirstNameError, firstNameError);
+  }
+});
+
+//Last name
+lastNameInput.addEventListener("input", () => {
+  if (textVerify(lastNameInput.value)) {
+    lastNameError.classList.add("hide");
+    validInput(lastNameInput);
+  } else {
+    errorUpdate(lastNameInput, lastNameError);
+    emptyUpdate(lastNameInput, emptyLastNameError, lastNameError);
+  }
+});
+
+//Phone
+phoneInput.addEventListener("input", () => {
+  if (phoneVerify(phoneInput.value)) {
+    phoneError.classList.add("hide");
+    validInput(phoneInput);
+  } else {
+    errorUpdate(phoneInput, phoneError);
+    emptyUpdate(phoneInput, emptyPhoneError, phoneError);
+  }
+});
+
+//Email
+emailInput.addEventListener("input", () => {
+  if (emailVerify(emailInput.value)) {
+    emailError.classList.add("hide");
+    validInput(emailInput);
+  } else {
+    errorUpdate(emailInput, emailError);
+    emptyUpdate(emailInput, emptyEmailError, emailError);
+  }
+});
+
+//Password
+passwordInput.addEventListener("input", () => {
+  if (passwordVerify(passwordInput.value)) {
+    passwordError.classList.add("hide");
+    validInput(passwordInput);
+  } else {
+    errorUpdate(passwordInput, passwordError);
+    emptyUpdate(passwordInput, emptyPasswordError, passwordError);
+  }
+});
+
+//Verify password
+verifyPasswordInput.addEventListener("input", () => {
+  if (verifyPasswordInput.value === passwordInput.value) {
+    verifyPasswordError.classList.add("hide");
+    validInput(verifyPasswordInput);
+  } else {
+    errorUpdate(verifyPasswordInput, verifyPasswordError);
+    emptyUpdate(passwordInput, emptyVerifyPasswordError, verifyPasswordError);
+  }
+});
+
+//Submit button
+submitButton.addEventListener("click", () => {
+  if (validClasses.length == 6 && invalidClasses.length == 0) {
+    alert("Success");
+  } else {
+    alert("Error");
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
